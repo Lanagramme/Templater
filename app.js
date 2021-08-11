d6fba26774998817a67b2caefa6bf570537b350e
Lanagramme <lborromee16@gmail.com>
2021-08-11 13:22:32 -0400

    correction des routes express

--- app.js
+++ app.js
@@ -3,13 +3,11 @@ const app = express()
 const port = 8000
 const path = require('path')
 
-app.get('/', (req,res) => {
-	res.sendFile(path.join(__dirname, './public/index.html'))
-})
+//app.get('/', (req,res) => {
+//	res.sendFile(path.join(__dirname, './public/index.html'))
+//})
 
-app.get('/app/:type/:file', (req,res) => {
-	res.sendFile(path.join(__dirname, './public'+req.url))
-})
+app.use('/', express.static(path.join(__dirname, './public')))
 
 app.listen(port, ()=>{
 	console.log(`port ${port} is open`)
